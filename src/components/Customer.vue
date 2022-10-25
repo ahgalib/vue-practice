57232cdacd24c58a622ee4d2b81a9ef2b1071fcb
user name <asadullahalgalib783@gmail.com>
2022-10-25 22:30:59 +0600

    props,install bootstrap and fontawesome

---
+++ src/components/Customer.vue
@@ -0,0 +1,18 @@
+<template>
+<h3>Hello Customer {{name}}</h3>
+</template>
+
+<script>
+export default{
+    name:'Customer',
+    props : {
+        name:{type:String,required:true}
+        age:{type:String,required:true}
+}
+}
+
+</script>
+
+<style>
+
+</style>
