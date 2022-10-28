64c127d1c4fddd5f0c756106890d594615efd616
user name <asadullahalgalib783@gmail.com>
2022-10-28 22:29:49 +0600

    component,counter,state,methods,router

--- src/App.vue
+++ src/App.vue
@@ -94,17 +94,90 @@ import Employee from './components/Employee.vue';
 import Counter from './components/Counter.vue';
 import Greeting from './components/Greeting.vue';
 import Form from './components/Form.vue';
+import Condition from './components/Condition.vue';
+import ForV from './components/ForV.vue';
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <h2 style="color:blue;">App Component</h2>
-  <button class="btn btn-primary"> Click</button>
-  <i class="fa fa-"></i>
-
+ 
+ <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+  <Navbar/>
+  <router-view/>
   <Customer name="Galib" age = "43" hobby="watching sports"/>
   <Employee/>
   <Counter/>
   <Greeting/>
   <Form/>
+  <Condition/>
+  <ForV/>
 </template>
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar navbar-dark bg-success navbar-expand-sm">
+        <div class="conatainer">
+            <p>Hello</p>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default{
+    name:"Navbar",
+}
+</script>
